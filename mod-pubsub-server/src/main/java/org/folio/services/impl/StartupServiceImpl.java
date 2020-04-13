a984ef286250533272e1ad8254646a443493e82b
Kateryna Senchenko <42532596+KaterynaSenchenko@users.noreply.github.com>
2020-04-13 18:11:59 +0300

    Added creating of topics on module startup (#60)
    (cherry picked from commit eb9669bb8db4249604664bfde127f32a00295ee5)

--- mod-pubsub-server/src/main/java/org/folio/services/impl/StartupServiceImpl.java
+++ mod-pubsub-server/src/main/java/org/folio/services/impl/StartupServiceImpl.java
@@ -7,6 +7,7 @@ import org.folio.kafka.KafkaConfig;
 import org.folio.rest.util.MessagingModuleFilter;
 import org.folio.rest.util.OkapiConnectionParams;
 import org.folio.services.ConsumerService;
+import org.folio.services.KafkaTopicService;
 import org.folio.services.StartupService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -18,19 +19,25 @@ import static org.folio.rest.jaxrs.model.MessagingModule.ModuleRole.SUBSCRIBER;
 @Component
 public class StartupServiceImpl implements StartupService {
 
+  private static final int NUMBER_OF_PARTITIONS = 1;
+  private static final short REPLICATION_FACTOR = 1;
+
   private Vertx vertx;
   private KafkaConfig kafkaConfig;
   private MessagingModuleDao messagingModuleDao;
   private ConsumerService consumerService;
+  private KafkaTopicService kafkaTopicService;
 
   public StartupServiceImpl(@Autowired Vertx vertx,
                             @Autowired KafkaConfig kafkaConfig,
                             @Autowired MessagingModuleDao messagingModuleDao,
-                            @Autowired ConsumerService consumerService) {
+                            @Autowired ConsumerService consumerService,
+                            @Autowired KafkaTopicService kafkaTopicService) {
     this.vertx = vertx;
     this.kafkaConfig = kafkaConfig;
     this.messagingModuleDao = messagingModuleDao;
     this.consumerService = consumerService;
+    this.kafkaTopicService = kafkaTopicService;
   }
 
   @Override
@@ -41,7 +48,9 @@ public class StartupServiceImpl implements StartupService {
           OkapiConnectionParams params = new OkapiConnectionParams(vertx);
           params.setOkapiUrl(kafkaConfig.getOkapiUrl());
           params.setTenantId(messagingModule.getTenantId());
-          consumerService.subscribe(Collections.singletonList(messagingModule.getEventType()), params);
+          kafkaTopicService.createTopics(Collections.singletonList(messagingModule.getEventType()),
+            messagingModule.getTenantId(), NUMBER_OF_PARTITIONS, REPLICATION_FACTOR)
+            .compose(ar -> consumerService.subscribe(Collections.singletonList(messagingModule.getEventType()), params));
         });
         return Future.succeededFuture();
       });
