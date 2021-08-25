4624954a3ac8dab7f10f3652ca3d279834b407a8
Ruslan Lavrov <47384893+RuslanLavrov@users.noreply.github.com>
2021-08-25 22:05:09 +0300

    KAFKAWRAP-2 - Take folio-kafka-wrapper lib out of mod-pubsub repository (#171)

--- folio-kafka-wrapper/src/main/java/org/folio/kafka/cache/exceptions/KafkaInternalCacheInitializationException.java
+++
@@ -1,8 +0,0 @@
-package org.folio.kafka.cache.exceptions;
-
-public class KafkaInternalCacheInitializationException extends RuntimeException{
-
-  public KafkaInternalCacheInitializationException(String message, Throwable cause) {
-    super(message, cause);
-  }
-}
