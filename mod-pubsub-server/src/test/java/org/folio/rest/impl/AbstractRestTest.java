a919e618f92988c6ad60c33adf4877890be11cb9
Sergiy Bichurin <sergiy_bichurin@epam.com>
2021-08-30 16:49:42 +0300

    changed expected status code from 400 to 404

--- mod-pubsub-server/src/test/java/org/folio/rest/impl/AbstractRestTest.java
+++ mod-pubsub-server/src/test/java/org/folio/rest/impl/AbstractRestTest.java
@@ -126,14 +126,14 @@ public abstract class AbstractRestTest {
               String error = res3.bodyAsJson(TenantJob.class).getError();
               // it would be better if this would actually succeed.. But we'll accept this error for now
               if (error != null) {
-                context.assertEquals(format("Failed to create %s user. Received status code 400",
+                context.assertEquals(format("Failed to create %s user. Received status code 404",
                   SYSTEM_USER_NAME), error);
               }
             }));
           } else {
             // if we get here and error is immediately returned from tenant init
             // it would be better if this would actually succeed.. But we'll accept this error for now
-            context.assertEquals(format("Failed to create %s user. Received status code 400",
+            context.assertEquals(format("Failed to create %s user. Received status code 404",
               SYSTEM_USER_NAME), res2.bodyAsString());
           }
         }));
