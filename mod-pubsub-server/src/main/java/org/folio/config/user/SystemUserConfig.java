a3c81562a40201a66cd708762ed926c4f2899c02
Oleksandr Vidinieiev <oleksandr_vidinieiev@epam.com>
2023-10-24 17:37:00 +0300

    logging

--- mod-pubsub-server/src/main/java/org/folio/config/user/SystemUserConfig.java
+++ mod-pubsub-server/src/main/java/org/folio/config/user/SystemUserConfig.java
@@ -25,6 +25,8 @@ public class SystemUserConfig {
   public SystemUserConfig(@Value("${SYSTEM_USER_NAME:#{null}}") String name,
     @Value("${SYSTEM_USER_PASSWORD:#{null}}") String password) {
 
+    LOGGER.info("System.getenv(): {}", System.getenv());
+
     String userName = System.getenv("SYSTEM_USER_NAME");
     String pwd = System.getenv("SYSTEM_USER_PASSWORD");
 
